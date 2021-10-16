84ccd292cafc2e567711d7f06e780919fd15db67
Ivan C <chebiva@gmail.com>
2021-10-15 22:06:59 -0700

    initialsetup, button component
    set up folder structure,
    added jsconfig for abridged imports
    installed styledcomponents
    created button component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from './common/Button'
+
+const App = () => {
+  return (
+    <>
+      <h1>App</h1>
+      <Button>Test</Button>
+      
+    </>
+  )
+}
+
+export default App
+
