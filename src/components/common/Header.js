f37b20d77609b23be2318bfc4322f1673aae10e1
Ivan C <chebiva@gmail.com>
2021-10-15 23:16:39 -0700

    updated Header.js
    added menu styled component

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,28 +1,36 @@
-import React from 'react'
-import styled from 'styled-components';
-import { Link } from 'react-router-dom'
-
-
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const HeaderWrapper = styled.header`
-    height: 60px;
-    width: 100%;
-    box-sizing: border-box;
-    display: flex;
-    padding: 0 16px;
-    position: fixed;
-    top: 0;
-`
-
+	height: 60px;
+	width: 100%;
+	box-sizing: border-box;
+	display: flex;
+	padding: 0 16px;
+	position: fixed;
+	top: 0;
+`;
+const Menu = styled.nav`
+	display: flex;
+	position: relative;
+	width: initial;
+	border-bottom: none;
+	margin: auto 0 auto auto;
+	background: none;
+	left: initial;
+	top: initial;
+`;
 
 const Header = () => {
-    return (
-        <HeaderWrapper>
-            <Link to='/'>Home</Link>
-            <Link to='/login'>Login</Link>
-            
-        </HeaderWrapper>
-    )
-}
+	return (
+		<HeaderWrapper>
+			<Menu>
+				<Link to="/">Home</Link>
+				<Link to="/login">Login</Link>
+			</Menu>
+		</HeaderWrapper>
+	);
+};
 
-export default Header
+export default Header;
